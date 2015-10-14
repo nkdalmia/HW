91ddd4e568de3e15b37c7afda256924d7e1c77dd
nkdalmia <nkdalmia@users.noreply.github.com>
2015-10-13 22:35:51 -0400

    reverted mystery and subject files

--- HW2/mystery.js
+++ HW2/mystery.js
@@ -1,60 +1,67 @@
 var fs = require("fs");
 
-function inc(p, q) {
-    if (q == undefined) q = 1;
+function inc(p, q){
+    if(q ==undefined) q =1;
 
-    if (p < 30) {
-        p = -p;
-    }
+   if( p < 30 )
+   {
+   	p = -p;
+   }
 
-    return p + q / q;
+    return p + q/q;
 }
 
 
-function fileTest(dir, filePath) {
-    if (!fs.existsSync(dir)) {
-        return false;
-    }
+function fileTest(dir, filePath)
+{
+	if (!fs.existsSync(dir)){
+   	return false;
+	}
 
-    if (fs.existsSync(filePath)) {
-        var buf = fs.readFileSync(filePath, "utf8");
-        if (buf.length > 0) {
-            return true;
-        }
-        return false;
-    }
+   if( fs.existsSync(filePath ))
+   {
+		var buf = fs.readFileSync(filePath, "utf8");
+		if( buf.length > 0 )
+		{
+			return true;
+		}
+		return false;
+	}
 }
 
 function normalize(phoneNumber) {
 
     return phoneNumber.replace(
-        /^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-        "$1$2$3"
+      /^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+      "$1$2$3"
     );
 
 }
 
-function format(phoneNumber, formatString, options) {
+function format(phoneNumber, formatString, options) 
+{
     // Normalize the phone number first unless not asked to do so in the options
     if (!options || !options.shouldNormalize) {
-        phoneNumber = normalize(phoneNumber)
+      phoneNumber = normalize(phoneNumber)
     };
 
-    for (var i = 0, l = phoneNumber.length; i < l; i++) {
-        formatString = formatString.replace("N", phoneNumber[i]);
+    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
+      formatString = formatString.replace("N", phoneNumber[i]);
     }
-
+  
     return formatString;
 
 }
 
-function blackListNumber(phoneNumber) {
-    var num = format(phoneNumber, "(NNN) NNN-NNNN");
-    var area = num.substring(1, 4);
-    if (area == "919") {
-        return true;
-    }
-    return false;
+function blackListNumber(phoneNumber)
+{
+	var num = format(phoneNumber, "(NNN) NNN-NNNN");
+	var area = num.substring(1,4);
+	if( area == "919" )
+	{
+		return true;
+	}
+	return false;
 }
 
 exports.fileTest = fileTest;
@@ -62,3 +69,4 @@ exports.normalize = normalize;
 exports.format = format;
 exports.inc = inc;
 exports.blackListNumber = blackListNumber;
+
